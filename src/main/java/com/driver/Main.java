969eb45b946642603cc35492986e8647901130b2
ramak <ramak@DESKTOP-8SV8CJ8.bbrouter>
2023-08-09 21:46:38 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,15 @@ package com.driver;
 
 public class Main {
   
+	public static void main(String[] args) {
+		
+		RWOnly obj = new RWOnly();
+		//obj.name = "RK";
+		
+		//Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+		//The field RWOnly.name is not visible
+		//at com.driver.Main.main(Main.java:8)
+		
+		obj.setName("RK");
+	}
 }
